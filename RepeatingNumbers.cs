fee0083b98e32cfb36a5646bd7d12f020943caa7
NSK <nsk.infy@gmail.com>
2022-02-20 18:12:21 +0530

    Add project files.

---
+++ RepeatingNumbers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class RepeatingNumbers
+    {
+        
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 1, 5, 4, 3,3, 3, 5, 6, 7, 2, 7, 6, 2 };
+            Console.WriteLine(string.Join(",", GetRepeatingNumbers(array,1)));
+            
+        }
+        public static int[] GetRepeatingNumbers(int[] array, int times)
+        {
+            int[] response = array.Where(x => array.Count(y => y == x) == times).Distinct().ToArray();
+            return response;
+        }
+    }
+}
