b5326433efdf57252a1e77afd70bbb492213ae33
NSK <nsk.infy@gmail.com>
2022-02-21 09:21:35 +0530

    Coin change program

--- CoinChangeProblem.cs
+++ CoinChangeProblem.cs
@@ -1,10 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace consoleApps
 {
     class CoinChangeProblem
     {
+        private static int[] input = { 1, 3, 5, 6 };
+        private static int amount = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, GetChanges()?.Select(x => $"Coin of {x.Key} - {x.Value}")));
+        }
+
+        static Dictionary<int, int> GetChanges()
+        {
+            input = input.OrderByDescending(x => x).ToArray();
+
+            Dictionary<int, int> result = new Dictionary<int, int>();
+            Func<KeyValuePair<int, int>, int> total = x => x.Key * x.Value;
+            var index = 0;
+            while (result.Sum(total) < amount)
+            {
+                if ((input[index] + result.Sum(total)) <= amount)
+                {
+                    if (!result.ContainsKey(input[index]))
+                        result.Add(input[index], 1);
+                    while ((input[index] + result.Sum(total)) <= amount)
+                    {
+                        result[input[index]] = result[input[index]] + 1;
+                    }
+                }
+                if (++index == input.Length)
+                    break;
+            }
+            return result;
+        }
     }
 }
--- RepeatingNumbers.cs
+++ RepeatingNumbers.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     class RepeatingNumbers
     {
         
-        static void Main(string[] args)
+        static void Main1(string[] args)
         {
             int[] array = new int[] { 1, 1, 5, 4, 3,3, 3, 5, 6, 7, 2, 7, 6, 2 };
             Console.WriteLine(string.Join(",", GetRepeatingNumbers(array,1)));
